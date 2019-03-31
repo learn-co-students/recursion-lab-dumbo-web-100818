f9d540af07b204eabc2031560a4e0fc15bf7db36
Alex Chao <chao.alexander.123@gmail.com>
2019-03-31 20:18:46 +0000

    define includesNumber function

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
 // Code your solution here!
+function printString(string) {
+  console.log(string[0])
+  if (string.length === 1) return true
+  printString(string.slice(1))
+}
 
+function reverseString(string) {
+  if (string.length === 1) return string
+  return string.slice(-1) + reverseString(string.slice(0, -1))
+}
+
+function isPalindrome(string) {
+  let result
+  let firstChar = string.slice(0, 1)
+  let lastChar = string.slice(-1)
+  if (string.length === 1 || (string.length === 2 && firstChar === lastChar)) {
+    result = true
+  } else if (firstChar === lastChar) {
+    result = isPalindrome(string.slice(1, -1))
+  } else {
+    result = false
+  }
+  return result
+}
+
+function addUpTo(array, idx) {
+  if (idx === 0) {
+    return array[0]
+  } else {
+    let currentNum = array[0]
+    let newIdx = idx - 1
+    let newArray = array.slice(1)
+    return currentNum + addUpTo(newArray, newIdx)
+  }
+}
+
+function compareNum(num1, num2) {
+  return num1 > num2 ? num1 : num2
+}
+
+function maxOf(array) {
+  if (array.length === 1) {
+    return array [0]
+  } else {
+    return compareNum(array.pop(), maxOf(array))
+  }
+}
+
+function includesNumber(array) {
+  
+}
